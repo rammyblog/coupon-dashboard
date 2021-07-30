dbf8d09f59b8ea1a99398825ea90906daaf88790
Babatunde Onasanya <onasanyatunde67@gmail.com>
2021-07-30 16:33:31 +0100

    Added coupon to dash

---
+++ src/components/dashboard/Coupon.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Coupon = () => {
+  return <div>Welcome to coupon page</div>;
+};
+
+export default Coupon;
--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,13 @@ import { Route, Switch } from "react-router-dom";
 import PrivateRoute from "./PrivateRoute";
 import Login from "./components/auth/Login";
 import Home from "./components/dashboard/Home";
+import Coupon from "./components/dashboard/Coupon";
 
 const BaseRouter = () => (
   <Switch>
     <Route exact path="/login" component={Login} />
-    {/* <PrivateRoute exact path="/profile/" component={Profile} /> */}
     <PrivateRoute exact path="/" component={Home} />
+    <PrivateRoute exact path="/coupon" component={Coupon} />
   </Switch>
 );
 
