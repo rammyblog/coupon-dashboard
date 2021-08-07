a495da397f34bf8bdcafeedc6353689ef8185dec
Babatunde Onasanya <onasanyatunde67@gmail.com>
2021-08-07 19:36:25 +0100

    Worked on getting a single coupon

--- src/components/dashboard/SingleCoupon.js
+++ src/components/dashboard/SingleCoupon.js
@@ -3,12 +3,12 @@ import QRCode from 'qrcode';
 import DashboardHOC from './DashboardHoc';
 import { Typography, Image, Button } from 'antd';
 import { useSelector, useDispatch } from 'react-redux';
-import {fetchSingleCoupons} from '../../store/coupon/coupoonActionCreators'
+import { fetchSingleCoupons } from '../../store/coupon/couponActionCreators';
 
 const { Title, Text } = Typography;
 const SingleCoupon = ({ match }) => {
   const dispatch = useDispatch();
-  const {singleCoupon} = useSelector(state => state.coupon);
+  const { singleCoupon, loading } = useSelector((state) => state.coupon);
   const code = match.params.code;
   const [qrcode, setQrCode] = useState();
   const generateQR = async (text) => {
@@ -19,28 +19,37 @@ const SingleCoupon = ({ match }) => {
     }
   };
   useEffect(() => {
-    fetchSingleCoupons(code);
-  }, [code]);
+    dispatch(fetchSingleCoupons(code));
+  }, [dispatch, code]);
   useEffect(() => {
     generateQR(code);
   }, [code]);
+  if (loading) {
+    return <p>Loading</p>;
+  }
   return (
     <>
-      <Button className="float-right">Edit Coupon</Button>
-      <div className="clear-both">
-        <Title level={5}>Coupon Code</Title>
-        <Text>CODE123</Text>
-        <Title level={5}>Description</Title>
-        <Text>Coupon for 50% off</Text>
-        <Title level={5}>Available</Title>
-        <Text>Yes</Text>
-        <Title level={5}>Redeemable from</Title>
-        <Text>15/12/2020</Text>
-        <Title level={5}>Redeemable To</Title>
-        <Text>15/12/2020</Text>
-        <Title level={5}>QR Code</Title>
-        <Image width={200} src={qrcode} />
-      </div>
+      {singleCoupon && (
+        <>
+          <Button className="float-right">Edit Coupon</Button>
+          <div className="clear-both">
+            <Title level={5}>Coupon Code</Title>
+            <Text>{singleCoupon.code}</Text>
+            <Title level={5}>Percentage off</Title>
+            <Text>{singleCoupon.percent_off}%</Text>
+            <Title level={5}>Description</Title>
+            <Text>{singleCoupon.description}</Text>
+            <Title level={5}>Available</Title>
+            <Text>{singleCoupon.description}</Text>
+            <Title level={5}>Redeemable from</Title>
+            <Text>{singleCoupon.redeem_from}</Text>
+            <Title level={5}>Redeemable To</Title>
+            <Text>{singleCoupon.redeem_to}</Text>
+            <Title level={5}>QR Code</Title>
+            <Image width={200} src={qrcode} />
+          </div>
+        </>
+      )}
     </>
   );
 };
